88afd8fb226db98c4986d610bc45c91dcebf5e30
TemoshoS <temoshomaduane@gmail.com>
2024-10-05 01:46:34 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import Home from './components/Home';
+import Bestpractice from './components/Bestpractice';
+import Complaints from './components/Complaints';
+import Dashboards from './components/Dashboards';
+import Footer from './components/Footer';
+import Reports from './components/Reports';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
