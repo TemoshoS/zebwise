7329cedd7f6ca81796db8720c84631bafc9a0ac7
MalwandlaShaun <hlungwanimalwandlashaun@gmail.com>
2024-10-06 05:04:05 +0200

    shaun - firebase configuration

--- src/components/Dashboards.js
+++ src/components/Dashboards.js
@@ -104,25 +104,54 @@ export default function Dashboards() {
     <SidebarComponent > </SidebarComponent>
      <div class="container d-flex bg-white">
          <div class="container pt-5  overflow-auto">
-             <div class='mb-5'>
+             <div class='mb-3'>
                  <p class='fs-3 fw-bold text-muted m-0 p-0'>Dashboard</p>
                  <p class='fs-6 text-muted'>Monitor the electricity yearly complaints and reports </p>
              </div>
+             <div class='d-flex align-items-center gap-2 mb-3'>
+                 <div className="col-lg-3 p-2">
+                     <div className="card shadow">
+                         <div className="card-body">
+                             <div className="d-flex align-content-center">
+                                 <div class='pe-3'><h5 className="card-title"> Total Reports</h5>
+                                     <h5 className="card-text bold-text text-muted">555</h5></div>
+                                 <img src='/small-graphs.png' alt={'small-graphs'}/>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+                 <div className="col-lg-3 p-2">
+                     <div className="card shadow">
+                         <div className="card-body">
+                             <div className="d-flex align-content-center">
+                                 <div class='pe-3'><h5 className="card-title"> Total Complaints</h5>
+                                     <h5 className="card-text bold-text text-muted">444</h5></div>
+                                 <img src='/small-graphs.png' alt={'small-graphs'}/>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+
+             </div>
+
+
              <div class=" bg-light p-0 m-0 mb-5">
-             <div class="card-header d-lg-flex justify-content-lg-between p-0 m-0">
-                 <ul class="nav cursor-pointer p-0 m-0">
-                     <li class="nav-item"  onclick={() => toggleView('reports')} >
-                         <a class="nav-link btn-primary px-5 p-3 text-uppercase fw-semibold text-primary " aria-current="page"  >
-                          Reports
-                         </a>
-                    </li>
-                    <li class="nav-item" onclick={() => toggleView('complaints')}>
-                         <a class="nav-link btn-primary px-5 p-3 text-uppercase fw-semibold text-primary " aria-current="page"  >
-                            Complaints
-                        </a>
-                    </li>
-                <li class="nav-item"  onclick={() => toggleView('electricity')}>
-                 <a class="nav-link btn-primary px-5 p-3 text-uppercase fw-semibold text-primary " aria-current="page"  >
+                 <div class="card-header d-lg-flex justify-content-lg-between p-0 m-0">
+                     <ul class="nav cursor-pointer p-0 m-0">
+                         <li class="nav-item" onclick={() => toggleView('reports')}>
+                             <a class="nav-link btn-primary px-5 p-3 text-uppercase fw-semibold text-secondary "
+                                aria-current="page">
+                                 Reports
+                             </a>
+                         </li>
+                         <li class="nav-item" onclick={() => toggleView('complaints')}>
+                             <a class="nav-link btn-primary px-5 p-3 text-uppercase fw-semibold text-secondary "
+                                aria-current="page">
+                                 Complaints
+                             </a>
+                         </li>
+                         <li class="nav-item" onclick={() => toggleView('electricity')}>
+                             <a class="nav-link btn-primary px-5 p-3 text-uppercase fw-semibold text-secondary " aria-current="page"  >
                  Electricity Supply
                 </a>
                 </li>
