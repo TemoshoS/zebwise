095eacba3a07553e5314a5ae739f8621d6b1b409
Enhlekhoza <dzimbaelsa3@gmail.com>
2024-11-03 23:13:32 +0200

    elsa-blogs

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -14,16 +14,16 @@ const Blogs = () => {
                                     <div className="col-md-6 wow slideInUp" key={index} data-wow-delay={`${0.1 + index * 0.5}s`}>
                                         <div className="blog-item bg-light rounded overflow-hidden">
                                             <div className="blog-img position-relative overflow-hidden">
-                                                <img className="img-fluid" src={`https://siyakha.co.za/images/2024/07/02/image_${index + 1}.jpeg`} alt="" />
-                                                <a className="position-absolute top-0 start-0 bg-dark text-white rounded-end mt-5 py-2 px-4" href="">Event</a>
+                                                <img className="img-fluid" src={`https://siyakha.co.za/images/2024/07/02/image_${index + 1}.jpeg`} alt={`Blog Post ${index + 1}`} />
+                                                <a className="position-absolute top-0 start-0 bg-dark text-white rounded-end mt-5 py-2 px-4" href="">Community Event</a>
                                             </div>
                                             <div className="p-4">
                                                 <div className="d-flex mb-3">
-                                                    <small className="me-3"><i className="far fa-user text-dark me-2"></i>John Doe</small>
-                                                    <small><i className="far fa-calendar-alt text-dark me-2"></i>01 Jan, 2045</small>
+                                                    <small className="me-3"><i className="far fa-user text-dark me-2"></i>Admin</small>
+                                                    <small><i className="far fa-calendar-alt text-dark me-2"></i>{new Date().toLocaleDateString()}</small>
                                                 </div>
-                                                <h4 className="mb-3">Blog Title {index + 1}</h4>
-                                                <p>Dolor et eos labore stet justo sed est sed sed sed dolor stet amet</p>
+                                                <h4 className="mb-3">How to Report Community Issues</h4>
+                                                <p>Learn how you can effectively report issues in your community to drive change.</p>
                                                 <a className="text-uppercase" href="">Read More <i className="bi bi-arrow-right"></i></a>
                                             </div>
                                         </div>
@@ -57,7 +57,7 @@ const Blogs = () => {
                             {/* Search Form Start */}
                             <div className="mb-5 wow slideInUp" data-wow-delay="0.1s">
                                 <div className="input-group">
-                                    <input type="text" className="form-control p-3" placeholder="Keyword" />
+                                    <input type="text" className="form-control p-3" placeholder="Search Reports" />
                                     <button className="btn btn-primary px-4"><i className="bi bi-search"></i></button>
                                 </div>
                             </div>
@@ -69,7 +69,7 @@ const Blogs = () => {
                                     <h3 className="mb-0">Categories</h3>
                                 </div>
                                 <div className="link-animated d-flex flex-column justify-content-start">
-                                    {['B-BBEE (6)', 'People (6)', 'Corporate Advisory (0)', 'African Markets (6)', 'SMME Development (5)', 'Development Funding (0)', 'Social Impact (1)', 'ESG (2)', 'Digital (0)', 'Strategic Projects & Innovation (0)', 'COVID-19 (0)'].map((category, index) => (
+                                    {['Waste Management (3)', 'Safety Concerns (2)', 'Community Events (1)', 'Health Issues (4)', 'Education (5)'].map((category, index) => (
                                         <a className="h5 fw-semi-bold bg-light rounded py-2 px-3 mb-2" href="#" key={index}>
                                             <i className="bi bi-arrow-right me-2"></i>{category}
                                         </a>
@@ -81,13 +81,13 @@ const Blogs = () => {
                             {/* Recent Post Start */}
                             <div className="mb-5 wow slideInUp" data-wow-delay="0.1s">
                                 <div className="section-title section-title-sm position-relative pb-3 mb-4">
-                                    <h3 className="mb-0" style={{ color: 'black' }}>Recent Post</h3>
+                                    <h3 className="mb-0" style={{ color: 'black' }}>Recent Posts</h3>
                                 </div>
                                 {/* Example of a recent post */}
                                 <div className="d-flex rounded overflow-hidden mb-3" style={{ backgroundColor: 'white' }}>
-                                    <img className="img-fluid" src="https://siyakha.co.za/images/2024/07/08/sustainable-development-goals-still-life_small.jpg" style={{ width: '100px', height: '100px', objectFit: 'cover', filter: 'grayscale(100%)' }} alt="" />
+                                    <img className="img-fluid" src="https://siyakha.co.za/images/2024/07/08/sustainable-development-goals-still-life_small.jpg" style={{ width: '100px', height: '100px', objectFit: 'cover', filter: 'grayscale(100%)' }} alt="Recent Post" />
                                     <a href="" className="h5 fw-semi-bold d-flex align-items-center" style={{ backgroundColor: 'white', color: 'black', padding: '10px', border: '1px solid black' }}>
-                                        Connecting the E to the S to drive the G
+                                        How We Can Improve Our Neighborhood
                                     </a>
                                 </div>
                                 {/* Add more recent posts here as needed */}
